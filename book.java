e67c68c442706307ec272e0c6d6d6e6938fec9c4
Sohail Hasware <kd3180@hotmail.com>
2015-10-24 11:51:18 +0200

    add book.java

---
+++ book.java
@@ -0,0 +1,11 @@
+package com.hascode.tutorial.cucumber.book;
+ 
+import java.util.Date;
+ 
+public class Book {
+	private final String title;
+	private final String author;
+	private final Date published;
+ 
+	// constructors, getter, setter ommitted
+}
